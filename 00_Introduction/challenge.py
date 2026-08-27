41b2eefe443c9b91725d44068e27a69a779a9d7b
Froyd <froydcarbajosa14@gmail.com>
2026-08-27 13:57:34 +0800

    feat: enhance challenge script with additional print statements and comments

--- 00_Introduction/challenge.py
+++ 00_Introduction/challenge.py
@@ -1,3 +1,4 @@
+# Challenge: Statements
 # Instructions
 # Inside the editor, complete the following steps:
 # Write a statement that prints "Hello World!"
@@ -6,4 +7,29 @@
 
 print("Hello World!")
 print("Have a good day.")
-print("Learning Python is fun!")
+print("Learning Python is fun!")    
+
+
+# Challenge: Output / Print
+# Instructions
+# Inside the editor, complete the following steps:
+# Print the text "I am" and the number 25 in one print statement
+
+print("I am", 25)
+
+# Challenge: Output / Print
+# Instructions
+# Inside the editor, complete the following steps:
+# Add a single-line comment that says This is a comment
+# Comment out the line print("This should not run") so it does not execute
+# Add a multiline comment (using triple quotes) that says This is a multiline comment
+
+# This is a comment
+# print("This should not run")
+
+"""
+This
+is
+multiline
+comment
+"""
