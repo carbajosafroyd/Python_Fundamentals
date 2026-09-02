b299d9de28cd2b4151d0df451e613e26b5fbf128
Froyd <froydcarbajosa14@gmail.com>
2026-09-02 23:28:36 +0800

    feat(casting): code challenge

---
+++ 04_Casting/challenge.py
@@ -0,0 +1,16 @@
+# # Challenge: Casting
+# Test your understanding of Python type casting by completing a small coding challenge.
+
+# Instructions
+# Inside the editor, complete the following steps:
+# Create a variable x with the integer value 1
+# Convert x to a float and store it in a
+# Convert x to a string and store it in b
+# Print a and b
+
+x = 1
+a = float(x)
+b = str(x)
+
+print(a)
+print(b)
